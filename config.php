f36cd9bc470f2ca9cce4e46968609135c546d0f9
Yassine Qoraiche <15586492+Qoraiche@users.noreply.github.com>
2024-04-21 21:10:21 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,29 +1,29 @@
-<?php return array (
-  'debug' => false,
-  'database' => 
-  array (
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'port' => 3306,
-    'database' => 'flarum',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'prefix' => 'flar',
-    'strict' => false,
-    'engine' => 'InnoDB',
-    'prefix_indexes' => true,
-  ),
-  'url' => 'http://flarum.test',
-  'paths' => 
-  array (
-    'api' => 'api',
-    'admin' => 'admin',
-  ),
-  'headers' => 
-  array (
-    'poweredByHeader' => true,
-    'referrerPolicy' => 'same-origin',
-  ),
+<?php return array(
+    'debug' => false,
+    'database' =>
+        array(
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'port' => 3306,
+            'database' => 'flarum',
+            'username' => 'flarum',
+            'password' => 'Fi9tboul01@',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => 'flar',
+            'strict' => false,
+            'engine' => 'InnoDB',
+            'prefix_indexes' => true,
+        ),
+    'url' => 'https://flarumxx.snapifycode.com',
+    'paths' =>
+        array(
+            'api' => 'api',
+            'admin' => 'admin',
+        ),
+    'headers' =>
+        array(
+            'poweredByHeader' => true,
+            'referrerPolicy' => 'same-origin',
+        ),
 );
